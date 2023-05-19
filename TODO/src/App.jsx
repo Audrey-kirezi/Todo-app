014734e412543b5c0064d907ebcb23caa7fad0eb
Audrey-kirezi <audreykirezi100@gmail.com>
2023-05-19 11:13:36 +0200

    First commit

---
+++ TODO/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { TodoWrapper } from './components/todoWrapper'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <TodoWrapper/>
+      
+    </>
+  )
+}
+
+export default App
